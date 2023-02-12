8ff2c2bf4c2ec48f318f93398cc72405a0edc6ce
Evgeniy Dmitriev <jeckd@mail.ru>
2023-02-12 17:51:39 +0100

    BL unit tests cover

--- CalcApp_Test/Calculator_Tests.cs
+++ CalcApp_Test/Calculator_Tests.cs
@@ -6,11 +6,42 @@ public class Calculator_Tests
     [SetUp]
     public void Setup()
     {
+        //No need setup
     }
 
     [Test]
-    public void Test1()
+    public void Add_Operation_returns_correct_result()
     {
-        Assert.Pass();
+        var result = Calculator.Calc(5, 5, OperationsEnum.ADD);
+        Assert.AreEqual(10, result.Value);
     }
+
+    [Test]
+    public void Odd_Operation_returns_correct_result()
+    {
+        var result = Calculator.Calc(5, 5, OperationsEnum.ODD);
+        Assert.AreEqual(0, result.Value);
+    }
+    
+    [Test]
+    public void Mul_Operation_returns_correct_result()
+    {
+        var result = Calculator.Calc(5, 5, OperationsEnum.MUL);
+        Assert.AreEqual(25, result.Value);
+    }
+
+    [Test]
+    public void Div_Operation_returns_correct_result()
+    {
+        var result = Calculator.Calc(5, 5, OperationsEnum.DIV);
+        Assert.AreEqual(1, result.Value);
+    }
+
+    [Test]
+    public void Div_Operation_returns_error_when_division_on_0()
+    {
+        var result = Calculator.Calc(5, 0, OperationsEnum.DIV);
+        Assert.IsNotEmpty(result.Message);
+    }
+
 }
