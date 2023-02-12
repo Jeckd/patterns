09517b0f345f8fd06a74da36182f24d5f42c309a
Evgeniy Dmitriev <jeckd@mail.ru>
2023-02-12 17:37:40 +0100

    Create Unit test project

---
+++ CalcApp_Test/Calculator_Tests.cs
@@ -0,0 +1,16 @@
+using patterns.BL;
+namespace CalcApp_Test;
+
+public class Calculator_Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
